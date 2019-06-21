22fdc8bb148a0e8b0ba9f6d2ec74250edaf46f04
dzakynorman <dzakynormansyah@gmail.com>
2019-06-21 21:49:57 +0700

    revisi blog, aplikasi, beranda, navbar, footer

--- application/views/pages/contact/v_home.php
+++ application/views/pages/contact/v_home.php
@@ -76,6 +76,7 @@ textarea:focus {
 <!-- Javascript start -->
 <script>
     $(document).ready(function(){
+        $('#contact').addClass('active');
 		$(".zm-nmbr").keypress(function (e) {
 			//if the letter is not digit then display error and don't type anything
 			if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
