01f598eb1a5a03c06e43226d76628973da13897f
dzakynorman <dzakynormansyah@gmail.com>
2019-06-27 13:26:41 +0700

    nambah baru

--- application/views/layout/v_footer.php
+++ application/views/layout/v_footer.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row">
             <div class="col-lg-5">
-                <div class="zm-d-block;">
+                <div class="zm-mb-10 zm-d-block ">
                     <img src="<?php echo base_url(); echo $dist_img; ?>logos/zoomin_logo_white.png" alt="" width="191" height="69">
                 </div>
                 <p class="zm-p text-white">Zoomin menyediakan solusi <i>gamification</i> untuk aplikasi
