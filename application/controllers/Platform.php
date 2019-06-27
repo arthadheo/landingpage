135c7a804a5a8486514e839f6355c99cbbddd236
Zo <madza204@gmail.com>
2019-06-27 11:45:27 +0700

    menyelesaikan focus tag

--- application/controllers/Platform.php
+++ application/controllers/Platform.php
@@ -11,14 +11,9 @@ class Platform extends CI_Controller
 	$data['title'] = 'Platform Gamification untuk Aplikasi HRIS Anda | Zoomin';
 	$data['meta_description'] = 'Platform gamification Zoomin akan memperkaya aplikasi HRIS yang Anda miliki. Terdapat berbagai macam elemen gamification yang bisa Anda gunakan';
 	$data['meta_keywords'] = 'aplikasi hris';
-	$this->output->cache(5);
 	$this->load->view('layout/v_header', $data);
     $this->load->view('pages/platform/platform.php');
 	$this->load->view('layout/v_footer');
 	}
 
-	public function delete_file_cache()
-	{
-		$this->output->delete_cache();
-	}
 }
